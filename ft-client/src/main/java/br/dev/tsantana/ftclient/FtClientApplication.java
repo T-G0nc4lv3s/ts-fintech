46dfe5194249d24f5e05e4cf2b4c3ede56e7b77b
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-02-13 16:29:36 -0300

    ft-client project created

---
+++ ft-client/src/main/java/br/dev/tsantana/ftclient/FtClientApplication.java
@@ -0,0 +1,13 @@
+package br.dev.tsantana.ftclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtClientApplication.class, args);
+	}
+
+}
