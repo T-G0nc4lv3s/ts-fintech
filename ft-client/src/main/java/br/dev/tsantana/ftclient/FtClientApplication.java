8ab2692ae7d5f4829ec1cca61c4825ace4c90caa
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-02-13 16:57:24 -0300

    Configuring eureka server client

--- ft-client/src/main/java/br/dev/tsantana/ftclient/FtClientApplication.java
+++ ft-client/src/main/java/br/dev/tsantana/ftclient/FtClientApplication.java
@@ -2,7 +2,9 @@ package br.dev.tsantana.ftclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class FtClientApplication {
 
